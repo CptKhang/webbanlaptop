9217feb36f6cd4156c21e553fd77e5486b43b9c6
CptKhang <cptkhang0108@gmail.com>
2023-12-19 15:58:26 +0700

    add gio hang + cap nhat gio hang

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
     <title>KShop</title>
 </head>
 <body>
     <div class="wrapper">
         <?php
+            session_start();
             include("admincp/config/config.php");
             include("pages/header.php");
             include("pages/menu.php");
--- pages/main/sanpham.php
+++ pages/main/sanpham.php
@@ -1,23 +1,24 @@
 <p>Chi tiet san pham</p>
 <?php
-    $sql_chitiet = "SELECT * FROM tbl_sanpham,tbl_danhmuc WHERE tbl_sanpham.id_danhmuc =tbl_danhmuc.id_danhmuc AND tbl_sanpham.id_sanpham='$_GET[id]' LIMIT 1";
-    $query_chitiet = mysqli_query($mysqli,$sql_chitiet);
-    while($row_chitiet = mysqli_fetch_array($query_chitiet)){
-?> 
+    $sql_chitiet = "SELECT * FROM tbl_sanpham,tbl_danhmuc WHERE tbl_sanpham.id_danhmuc=tbl_danhmuc.id_danhmuc AND tbl_sanpham.id_sanpham='$_GET[id]' LIMIT 1";
+	$query_chitiet = mysqli_query($mysqli,$sql_chitiet);
+	while($row_chitiet = mysqli_fetch_array($query_chitiet)){
+?>
 <div class="wrapper_chitiet">
-    <div class="hinhanh_sanpham">
-            <img width="80%" src="admincp/modules/quanlysp/uploads/<?php echo $row_chitiet['hinhanh']?>">
-    </div>
-        <form method="POST" action="pages/main/themgiohang.php?idsanpham=<?php echo $row_chitiet['id_sanpham'] ?>">
-            <div class="chitiet_sanpham">
-                    <h3 style="margin:0">Tên sản phẩm: <?php echo $row_chitiet['tensanpham'] ?></h3>
-                    <h3>Mã sản phẩm: <?php echo $row_chitiet['masp'] ?></h3>
-                    <h3>Giá sản phẩm: <?php echo number_format($row_chitiet['giasp'],0,',','.').'vnd' ?></h3>
-                    <h3>Số lượng sản phẩm: <?php echo $row_chitiet['soluong'] ?></h3>
-                    <h3>Danh mục sản phẩm: <?php echo $row_chitiet['tendanhmuc'] ?></h3>
-                    <p><input class="themgiohang" type="submit" value="Thêm giỏ hàng"></p>
-            </div>
-        </form>
+	<div class="hinhanh_sanpham">
+		<img width="100%" src="admincp/modules/quanlysp/uploads/<?php echo $row_chitiet['hinhanh'] ?>">
+	</div>
+	<form method="POST" action="pages/main/themgiohang.php?idsanpham=<?php echo $row_chitiet['id_sanpham'] ?>">
+		<div class="chitiet_sanpham">
+			<h3 style="margin:0">Tên sản phẩm : <?php echo $row_chitiet['tensanpham'] ?></h3>
+			<p>Mã sp: <?php echo $row_chitiet['masp'] ?></p>
+			<p>Giá sp: <?php echo number_format($row_chitiet['giasp'],0,',','.').'vnđ' ?></p>
+			<p>Số lượng sp: <?php echo $row_chitiet['soluong'] ?></p>
+			<p>Danh mục sp: <?php echo $row_chitiet['tendanhmuc'] ?></p>
+			<p><input class="themgiohang" name="themgiohang" type="submit" value="Thêm giỏ hàng"></p>
+			
+		</div>
+	</form>
 </div>
 <?php
 }
